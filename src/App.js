564bd411ee97d287851a66f2bca713794c77d6dd
Jar'Zeno <jarzenojarrett@gmail.com>
2023-03-20 08:14:36 -0700

    Set up header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <p>Hello Zeno</p>
+      {/* 
+       Header
+       Home
+       Portfolio
+       About
+       Contact
+       Footer
+      */}
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import { React } from "react";
+
+const Header = () => {
+    return (
+        <nav
+            id="home"
+            className="flex justify-between pt-6 px-16 text-4xl font-bold"
+        >
+            <a className="hover:underline" href="#home">J. Steven Jarrett</a>
+            <ul
+                className="flex gap-8"
+            >
+                <li className="hover:underline"><a href="#Portfolio">Portfolio</a></li>
+                <li className="hover:underline"><a href="#About">About</a></li>
+                <li className="hover:underline"><a href="#Contact">Contact</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Header;
