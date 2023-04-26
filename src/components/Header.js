dd2c51167bbc7b20246bc540f8f63dd7b7d7591b
Jar'Zeno <jarzenojarrett@gmail.com>
2023-04-26 12:48:41 -0700

    Add skills section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,23 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun } from "@fortawesome/free-regular-svg-icons";
 import { React } from "react";
 
 const Header = () => {
     return (
         <nav
-            className="flex justify-center items-center pt-6 px-16 text-4xl font-bold mb-12"
+            className="flex justify-end items-center pt-6 px-16 text-4xl font-bold mb-12"
         >
             <ul
                 className="flex gap-8"
             >
-                <li className="hover:underline hover:text-neutral-500"><a href="#Portfolio">Portfolio</a></li>
-                <li className="hover:underline hover:text-neutral-500"><a href="#Blog">Blog</a></li>
-                <li className="hover:underline hover:text-neutral-500"><a href="#Contact">Contact</a></li>
+                <li>
+                    <button>
+                        <FontAwesomeIcon icon={faSun}/>
+                    </button>
+                </li>
+                <li className="hover:underline hover:text-neutral-500"><a href="#Portfolio">P</a></li>
+                <li className="hover:underline hover:text-neutral-500"><a href="#Blog">B</a></li>
+                <li className="hover:underline hover:text-neutral-500"><a href="#Contact">C</a></li>
             </ul>
         </nav>
     )
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,67 +1,111 @@
 import React from "react";
 import pic from "../img/my_pic.jpg";
-import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin, faGithub, faHtml5, faCss3, faJs, faNodeJs, faReact, faGit, faFigma, faGitAlt, faNpm, } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTerminal } from "@fortawesome/free-solid-svg-icons";
+import expressIcon from "../icons/express.svg";
+import jestIcon from "../icons/jest.svg";
+import mongodbIcon from "../icons/mongoDB.svg";
+import tailwindCSSIcon from "../icons/tailwindCSS.svg";
+import vscodeIcon from "../icons/vscode.svg";
 
 const Home = () => {
     return (
         <div
             id="Portfolio"
-            className="flex flex-col items-center justify-center"
         >
             <div
-                className="text-center max-w-xl font-bold"
+                id="Portfolio-About"
+                className="flex items-center justify-between max-w-3xl mx-auto"
             >
-                <p
-                    className="text-4xl mb-1"    
+                <div
+                    className="flex flex-col items-center justify-center text-center max-w-xl font-bold"
                 >
-                    J. Steven Jarrett
-                </p>
-                <p
-                    className="text-xl mb-1"  
-                >
-                    Software Engineer &amp; Full Stack Developer
-                </p>
-                <p
-                    className=" text-sm"
-                >
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-            </div>
+                    <p
+                        className="text-4xl mb-1"    
+                    >
+                        J. Steven Jarrett
+                    </p>
+                    <p
+                        className="text-xl mb-1"  
+                    >
+                        Software Engineer &amp; Full Stack Developer
+                    </p>
+                    <p
+                        className="text-sm"
+                    >
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                    <div
+                        id="portfolio-social-links"
+                        className="flex gap-3"
+                    >
+                        <a href="mailto:stevenjarrett26@gmail.com">
+                            <FontAwesomeIcon 
+                                icon={faEnvelope} 
+                                className="hover:text-neutral-500"
+                                size="2x"
+                            />
+                        </a>
+                        <a href="https://www.linkedin.com/in/steven-jarrett-b52296263/" target="_blank" rel="noreferrer">
+                            <FontAwesomeIcon   
+                                icon={faLinkedin}
+                                className="hover:text-neutral-500"
+                                size="2x" 
+                            />
+                        </a>
+                        <a href="https://github.com/Bantchee" target="_blank" rel="noreferrer">
+                            <FontAwesomeIcon 
+                                icon={faGithub} 
+                                className="hover:text-neutral-500"
+                                size="2x"
+                            />
+                        </a>
+                    </div>
+                </div>
             
-            <img 
-                src={pic} 
-                alt="J. Steven Jarrett" 
-                className="w-48 rounded-full"
-            />
+                <div>
+                <img 
+                    src={pic} 
+                    alt="J. Steven Jarrett" 
+                    className="w-36"
+                />
+                </div>
+            </div>
             <div
-                className="flex gap-3"
+                id="Portfolio-Projects"
+                className="flex flex-col items-center max-w-6xl mx-auto"
             >
-                <a href="mailto:stevenjarrett26@gmail.com">
-                    <FontAwesomeIcon 
-                        icon={faEnvelope} 
-                        className="hover:text-neutral-500"
-                        size="2x"
-                    />
-                </a>
-                <a href="https://www.linkedin.com/in/steven-jarrett-b52296263/" target="_blank" rel="noreferrer">
-                    <FontAwesomeIcon   
-                        icon={faLinkedin}
-                        className="hover:text-neutral-500"
-                        size="2x" 
-                    />
-                </a>
-                <a href="https://github.com/Bantchee" target="_blank" rel="noreferrer">
-                    <FontAwesomeIcon 
-                        icon={faGithub} 
-                        className="hover:text-neutral-500"
-                        size="2x"
-                    />
-                </a>
+                <div className="flex items-center gap-4 font-bold">
+                    <p className="text-2xl mb-1">Languages |</p>
+                    <FontAwesomeIcon icon={faHtml5} size="2x" className="text-orange-500" title="HTML5"/>
+                    <FontAwesomeIcon icon={faCss3} size="2x"  className="text-blue-500" title="CSS3"/>
+                    <FontAwesomeIcon icon={faJs} size="2x" className="text-yellow-500" title="JavaScript"/>
+                </div>
+                <div className="flex items-center gap-4 font-bold">
+                    <p className="text-2xl mb-1">Technologies |</p>
+                    <FontAwesomeIcon icon={faNodeJs} size="2x" className="text-green-500" title="Node.js"/>
+                    <FontAwesomeIcon icon={faReact} size="2x" className="text-blue-500" title="React.js"/>
+                    <img src={expressIcon} alt="express icon" className="w-10" title="Express.js"/>
+                    <img src={mongodbIcon} alt="mongoDB icon" className="w-10" title="MongoDB"/>
+                    <img src={tailwindCSSIcon} alt="tailwind css icon" className="w-10" title="Tailwind CSS"/>
+                    <img src={jestIcon} alt="jest icon" className="w-10" title="Jest"/>
+                </div>
+                <div className="flex items-center gap-4 font-bold">
+                    <p className="text-2xl mb-1">Tools |</p>
+                    <img src={vscodeIcon} alt="jest icon" className="w-10 fill-blue-500" title="VSCode"/>
+                    <FontAwesomeIcon icon={faNpm} size="2x" className="text-red-500" title="npm"/>
+                    <FontAwesomeIcon icon={faGitAlt} size="2x" className="text-orange-500" title="Git"/>
+                    <FontAwesomeIcon icon={faGithub} size="2x" className="text-black" title="GitHub"/>
+                    <FontAwesomeIcon icon={faFigma} size="2x" className="text-purple-500" title="Figma"/>
+                    <FontAwesomeIcon icon={faTerminal} size="2x" className="text-black" title="Terminal"/>
+                </div>
+                {/* Set Up to pull from JSON / Database */}
+
             </div>
         </div>
     )
