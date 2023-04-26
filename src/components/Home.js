0f45acb9f5e07ce2392d33a6b77b10d79f50b8c3
Jar'Zeno <jarzenojarrett@gmail.com>
2023-04-26 10:29:34 -0700

    Update Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import pic from "../img/my_pic.jpg";
-import { faTwitter, faInstagram, faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -8,20 +8,60 @@ const Home = () => {
     return (
         <div
             id="Home"
-            className="flex flex-col items-center justify-center text-2xl font-bold"
+            className="flex flex-col items-center justify-center"
         >
-            <p>Front End Engineer</p>
+            <div
+                className="text-center max-w-xl font-bold"
+            >
+                <p
+                    className="text-4xl mb-1 hover:text-neutral-500"    
+                >
+                    J. Steven Jarrett
+                </p>
+                <p
+                    className="text-xl mb-1"  
+                >
+                    Software Engineer &amp; Full Stack Developer
+                </p>
+                <p
+                    className=" text-sm"
+                >
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
+            
             <img 
                 src={pic} 
                 alt="J. Steven Jarrett" 
                 className="w-48 rounded-full"
             />
-            <div>
-                <a href="#"><FontAwesomeIcon icon={faEnvelope} /></a>
-                <a href="#"><FontAwesomeIcon icon={faTwitter} /></a>
-                <a href="#"><FontAwesomeIcon icon={faInstagram} /></a>
-                <a href="#"><FontAwesomeIcon icon={faLinkedin} /></a>
-                <a href="#"><FontAwesomeIcon icon={faGithub} /></a>
+            <div
+                className="flex gap-3"
+            >
+                <a href="mailto:stevenjarrett26@gmail.com">
+                    <FontAwesomeIcon 
+                        icon={faEnvelope} 
+                        className="hover:text-neutral-500"
+                        size="2x"
+                    />
+                </a>
+                <a href="https://www.linkedin.com/in/steven-jarrett-b52296263/" target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon   
+                        icon={faLinkedin}
+                        className="hover:text-neutral-500"
+                        size="2x" 
+                    />
+                </a>
+                <a href="https://github.com/Bantchee" target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon 
+                        icon={faGithub} 
+                        className="hover:text-neutral-500"
+                        size="2x"
+                    />
+                </a>
             </div>
         </div>
     )
