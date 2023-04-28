2034f47582b9fcaaac8b4764b34f4326dbce8c42
Jar'Zeno <jarzenojarrett@gmail.com>
2023-04-28 11:41:10 -0700

    Implement boilerplate for components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from "react";
 import Header from "./components/Header";
 import Portfolio from "./components/Portfolio";
+import Blog from "./components/Blog";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div id="App" className="my-8">
       <Header />
       <Portfolio />
-      {/* 
-       Home
-       Portfolio
-       About
-       Contact
-       Footer
-      */}
+      <Blog />
+      <Contact />
+      <Footer />
     </div>
   );
 }
