53ee6d8601dbefcc3b40f0fd3b936bc8c9756223
Jar'Zeno <jarzenojarrett@gmail.com>
2023-04-28 11:59:20 -0700

    Edit about section

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -13,29 +13,14 @@ const Home = () => {
     return (
         <div id="Portfolio" className="flex flex-col gap-4">
             <div id="About" className="flex flex-col items-center max-w-6xlmx-auto">
-                <div className="flex flex-col items-center justify-center text-center max-w-xl font-bold" >
-                    <p
-                        className="text-4xl mb-1"    
-                    >
+                <div className="flex flex-col items-center justify-center text-center max-w-xl" >
+                    <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">
                         J. Steven Jarrett
-                    </p>
-                    <p
-                        className="text-xl mb-1"  
-                    >
+                    </h1>
+                    <p className="text-base md:text-xl mb-3 font-medium">
                         Software Engineer &amp; Full Stack Developer
                     </p>
-                    <p
-                        className="text-sm"
-                    >
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                    <div
-                        id="portfolio-social-links"
-                        className="flex gap-3"
-                    >
+                    <div id="portfolio-social-links" className="flex gap-3">
                         <a href="mailto:stevenjarrett26@gmail.com">
                             <FontAwesomeIcon 
                                 icon={faEnvelope} 
@@ -63,6 +48,7 @@ const Home = () => {
             </div>
             
             <div id="Projects" className="flex flex-col items-center gap-4">
+                {/* Set Up to pull from JSON / Database */}
                 <div className="flex items-center justify-center gap-6">
                     <div className=" w-72 h-48 bg-neutral-500">
                         <a href="#">Site</a>
@@ -77,39 +63,39 @@ const Home = () => {
                         <a href="#">Code</a>
                     </div>
                 </div>
+
+                <div id="Skills" className="flex flex-col items-center max-w-6xl mx-auto">
+                    <div className="flex items-center gap-4 font-bold">
+                        <p className="text-2xl mb-1">Languages |</p>
+                        <FontAwesomeIcon icon={faHtml5} size="2x" className="text-orange-500" title="HTML5"/>
+                        <FontAwesomeIcon icon={faCss3} size="2x"  className="text-blue-500" title="CSS3"/>
+                        <FontAwesomeIcon icon={faJs} size="2x" className="text-yellow-500" title="JavaScript"/>
+                    </div>
+                    <div className="flex items-center gap-4 font-bold">
+                        <p className="text-2xl mb-1">Technologies |</p>
+                        <FontAwesomeIcon icon={faNodeJs} size="2x" className="text-green-500" title="Node.js"/>
+                        <FontAwesomeIcon icon={faReact} size="2x" className="text-blue-500" title="React.js"/>
+                        <img src={expressIcon} alt="express icon" className="w-10" title="Express.js"/>
+                        <img src={mongodbIcon} alt="mongoDB icon" className="w-10" title="MongoDB"/>
+                        <img src={tailwindCSSIcon} alt="tailwind css icon" className="w-10" title="Tailwind CSS"/>
+                        <img src={jestIcon} alt="jest icon" className="w-10" title="Jest"/>
+                    </div>
+                    <div className="flex items-center gap-4 font-bold">
+                        <p className="text-2xl mb-1">Tools |</p>
+                        <img src={vscodeIcon} alt="jest icon" className="w-10 fill-blue-500" title="VSCode"/>
+                        <FontAwesomeIcon icon={faNpm} size="2x" className="text-red-500" title="npm"/>
+                        <FontAwesomeIcon icon={faGitAlt} size="2x" className="text-orange-500" title="Git"/>
+                        <FontAwesomeIcon icon={faGithub} size="2x" className="text-black" title="GitHub"/>
+                        <FontAwesomeIcon icon={faFigma} size="2x" className="text-purple-500" title="Figma"/>
+                        <FontAwesomeIcon icon={faTerminal} size="2x" className="text-black" title="Terminal"/>
+                    </div>
+                </div>
+
                 <div>
-                    <a href="https://github.com/Bantchee" target="_blank" rel="noreferrer" className="bg-black rounded-md text-white font-bold p-2 text-xl">More Projects</a>
+                        <a href="https://github.com/Bantchee" target="_blank" rel="noreferrer" className="bg-black rounded-md text-white font-bold p-2 text-xl">More Projects</a>
                 </div>
             </div>
 
-            <div id="Skills" className="flex flex-col items-center max-w-6xl mx-auto">
-                <div className="flex items-center gap-4 font-bold">
-                    <p className="text-2xl mb-1">Languages |</p>
-                    <FontAwesomeIcon icon={faHtml5} size="2x" className="text-orange-500" title="HTML5"/>
-                    <FontAwesomeIcon icon={faCss3} size="2x"  className="text-blue-500" title="CSS3"/>
-                    <FontAwesomeIcon icon={faJs} size="2x" className="text-yellow-500" title="JavaScript"/>
-                </div>
-                <div className="flex items-center gap-4 font-bold">
-                    <p className="text-2xl mb-1">Technologies |</p>
-                    <FontAwesomeIcon icon={faNodeJs} size="2x" className="text-green-500" title="Node.js"/>
-                    <FontAwesomeIcon icon={faReact} size="2x" className="text-blue-500" title="React.js"/>
-                    <img src={expressIcon} alt="express icon" className="w-10" title="Express.js"/>
-                    <img src={mongodbIcon} alt="mongoDB icon" className="w-10" title="MongoDB"/>
-                    <img src={tailwindCSSIcon} alt="tailwind css icon" className="w-10" title="Tailwind CSS"/>
-                    <img src={jestIcon} alt="jest icon" className="w-10" title="Jest"/>
-                </div>
-                <div className="flex items-center gap-4 font-bold">
-                    <p className="text-2xl mb-1">Tools |</p>
-                    <img src={vscodeIcon} alt="jest icon" className="w-10 fill-blue-500" title="VSCode"/>
-                    <FontAwesomeIcon icon={faNpm} size="2x" className="text-red-500" title="npm"/>
-                    <FontAwesomeIcon icon={faGitAlt} size="2x" className="text-orange-500" title="Git"/>
-                    <FontAwesomeIcon icon={faGithub} size="2x" className="text-black" title="GitHub"/>
-                    <FontAwesomeIcon icon={faFigma} size="2x" className="text-purple-500" title="Figma"/>
-                    <FontAwesomeIcon icon={faTerminal} size="2x" className="text-black" title="Terminal"/>
-                </div>
-                {/* Set Up to pull from JSON / Database */}
-
-            </div>
         </div>
     )
 }
