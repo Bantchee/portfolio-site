edb0c80312526223fed960c9b509ced47280c76a
Jar'Zeno <jarzenojarrett@gmail.com>
2023-04-29 07:33:40 -0700

    Version 1 of site

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from "react";
-import Header from "./components/Header";
 import Portfolio from "./components/Portfolio";
-import Blog from "./components/Blog";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
 
 function App() {
   return (
     <div id="App" className="my-8">
-      <Header />
       <Portfolio />
-      <Blog />
-      <Contact />
-      <Footer />
     </div>
   );
 }
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -14,34 +14,26 @@ const ProjectItem = ({title, description, imgUrl, siteLink, codeLink}) => {
                 alt="portfolio project"
                 className="w-full h-36 md:h-48 object-cover cursor-ponter rounded-md"
             />
-            <div className="w-full">
-                <h3>{title}</h3>
-                {(displayDescription) ? 
-                    <div>
-                        <p>{description}</p>
-                        <div
-                            className="flex gap-1"
-                        >
-                            <a 
-                                href={siteLink}  
-                                target="_blank" 
-                                rel="noreferrer noopener" 
-                                className="hover:underline font-bold border-2 border-black rounded-md px-1"
-                            >
-                                Site
-                            </a>
-                            <a 
-                                href={codeLink}  
-                                target="_blank" 
-                                rel="noreferrer noopener" 
-                                className="hover:underline font-bold border-2 border-black rounded-md px-1"
-                            >
-                                Code
-                            </a> 
-                        </div>
-                    </div> : null}
-                
-                
+            <div className="w-full flex flex-col items-center">
+                <h3 className="font-bold text-xl">{title}</h3>
+                <div className="flex gap-2">
+                    <a 
+                        href={siteLink}  
+                        target="_blank" 
+                        rel="noreferrer noopener" 
+                        className="hover:underline font-bold border-2 border-black rounded-md px-1"
+                    >
+                        Site
+                    </a>
+                    <a 
+                        href={codeLink}  
+                        target="_blank" 
+                        rel="noreferrer noopener" 
+                        className="hover:underline font-bold border-2 border-black rounded-md px-1"
+                    >
+                        Code
+                    </a> 
+                </div>
             </div>
         </div>
         
